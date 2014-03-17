fdca1c635bc96fced90de661f44ff0cf069cc79f
stone <stoneiswang@gmail.com>
2014-03-17 14:23:32 +0800

    Initial Commit

---
+++ TimeManager/TMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMAppDelegate.h
+//  TimeManager
+//
+//  Created by wangyipu on 14-3-17.
+//  Copyright (c) 2014年 stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimeManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimeManager
+//
+//  Created by wangyipu on 14-3-17.
+//  Copyright (c) 2014年 stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
